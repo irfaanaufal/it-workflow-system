2f92730bdad6e1362f11ddb4d85738f5e550fdcd
irfaanaufal <irfaanaufal13@gmail.com>
2026-07-01 11:09:16 +0700

    chore: add favicon and update layout icon link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
 
         <title inertia>{{ config('app.name', 'SINDANGASIH MAKMUR') }}</title>
 
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet" />
 
